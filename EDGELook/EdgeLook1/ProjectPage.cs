4381e860e68206cbb33251544cb8de0b06cc16d3
ShazmeenaZafar <nenekhan@ymail.com>
2019-02-15 16:32:18 -0800

    GUI for Manjot

--- EDGELook/EdgeLook1/ProjectPage.cs
+++ EDGELook/EdgeLook1/ProjectPage.cs
@@ -26,5 +26,40 @@ namespace EdgeLook1
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
