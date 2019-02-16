689eb934fe27cb546074d727bb30d14c5ec5f9dc
manjot3 <47119902+manjot3@users.noreply.github.com>
2019-02-15 16:34:29 -0800

    GUI edit (for Shaz)

--- EDGELook/EdgeLook1/ProjectPage.cs
+++ EDGELook/EdgeLook1/ProjectPage.cs
@@ -27,7 +27,41 @@ namespace EdgeLook1
 
         }
 
+<<<<<<< HEAD
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+=======
         private void label4_Click(object sender, EventArgs e)
+>>>>>>> d0c24046e4fbfb14eebfa57079a21f4930daef65
         {
 
         }
