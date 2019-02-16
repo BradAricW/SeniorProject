3ad7e2ae4ed9135115c7e5f719db1332deeb1c2a
ShazmeenaZafar <nenekhan@ymail.com>
2019-02-15 16:44:05 -0800

    GUI for Manjot

--- EDGELook/EdgeLook1/Form1.Designer.cs
+++ EDGELook/EdgeLook1/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Test";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- EDGELook/EdgeLook1/Form1.cs
+++ EDGELook/EdgeLook1/Form1.cs
@@ -16,5 +16,10 @@ namespace EdgeLook1
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
