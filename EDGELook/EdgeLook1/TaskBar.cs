b4798a1a0a4ee6a38a9e6a9e6a28d6727fee9b1d
alpokorney <alpokorney@gmail.com>
2019-02-18 10:40:41 -0800

    Update and rename Form1.cs to TaskBar.cs

--- EDGELook/EdgeLook1/TaskBar.cs
+++ EDGELook/EdgeLook1/TaskBar.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace TaskBar
 {
-    public partial class Form1 : Form
+    public partial class TaskBar : Form
     {
         bool isSideMenuPanelOpen = false;
 
