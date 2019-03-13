92c4c1828642559782e6a30ff8bba041d67933ec
jicorsini <31625513+jicorsini@users.noreply.github.com>
2019-03-13 16:57:10 -0700

    Added classes for profile page and login page

---
+++ EDGELook/EDGELook/EDGELook/EDGELook/ProfilePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDGELook
+{
+    class ProfilePage
+    {
+        private int myHours;
+        private DBConn Conn;
+
+        public void EditMyHours(TextBox hoursBox)
+        {
+            Conn = new DBConn();
+            myHours = int.Parse(hoursBox.Text);
+            Conn.editHours(myHours);
+        }
+    }
+}
