90f35441af46e150430b4f2d9220336f5d782bc9
BradAricW <45126866+BradAricW@users.noreply.github.com>
2019-05-03 14:59:44 -0700

    Revert "Merge branch 'master' of https://github.com/BradAricW/SeniorProject"
    This reverts commit 761c31a36ab969412184abc808f8abe3823bc61b, reversing
    changes made to 5fd6551e6d8c06264239c7c6896b5834b6290d0f.

--- EDGELook/EDGELook/EDGELook/EDGELook/AdminPage.cs
+++ EDGELook/EDGELook/EDGELook/EDGELook/AdminPage.cs
@@ -42,7 +42,6 @@ namespace EDGELook
             String lname = lastName.Text;
             String userName = email.Text;
             String phoneNumber = phone.Text;
-            String eID = employeeID.Text;
             int isAdmin;
             if (admin.Checked)
             {
@@ -69,7 +68,9 @@ namespace EDGELook
             {
                 Console.WriteLine(ex.Message);
             }
-         
+
+            String eID;
+            eID = employeeID.Text;
             try
             {
                 conn.Open();
@@ -88,26 +89,21 @@ namespace EDGELook
                 }
                 else
                 {
-                    if (eID == null)
-                    {
-                        MessageBox.Show("Enter an employee ID");
-                    }
-                    else
-                    {
-                        String insertEmp = "INSERT INTO Employee VALUES (?val1, ?val2, ?val3, ?val4, ?val5, ?val6, ?val7, ?val8, ?val9);";
-                        MySqlCommand cmd = new MySqlCommand(insertEmp, conn);
-                        cmd.Parameters.AddWithValue("?val1", eID);
-                        cmd.Parameters.AddWithValue("?val2", fname);
-                        cmd.Parameters.AddWithValue("?val3", lname);
-                        cmd.Parameters.AddWithValue("?val4", userName);
-                        cmd.Parameters.AddWithValue("?val5", defaultPassword);
-                        cmd.Parameters.AddWithValue("?val6", phoneNumber);
-                        cmd.Parameters.AddWithValue("?val7", defaultHours);
-                        cmd.Parameters.AddWithValue("?val8", isAdmin);
-                        cmd.Parameters.AddWithValue("?val9", isActive);
-                        cmd.ExecuteNonQuery();
-                        MessageBox.Show("Employee Added" + eID);
-                    }
+                    //String insertEmp = "INSERT INTO Employee VALUES ('" + eID + "', '" + fname + "', '" + lname + "', '" + userName + "', SHA2('" + defaultPassword + "', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), '" + phoneNumber + "', '" + hours + "', '" + isAdmin + "', '" + isActive + "');";
+                    //MySqlCommand cmd = new MySqlCommand(insertEmp, conn);
+                    string insertEmp = "INSERT INTO Employee VALUES (?val1, ?val2, ?val3, ?val4, ?val5, ?val6, ?val7, ?val8, ?val9);";
+                    MySqlCommand cmd = new MySqlCommand(insertEmp, conn);
+                    cmd.Parameters.AddWithValue("?val1", eID);
+                    cmd.Parameters.AddWithValue("?val2", fname);
+                    cmd.Parameters.AddWithValue("?val3", lname);
+                    cmd.Parameters.AddWithValue("?val4", userName);
+                    cmd.Parameters.AddWithValue("?val5", defaultPassword);
+                    cmd.Parameters.AddWithValue("?val6", phoneNumber);
+                    cmd.Parameters.AddWithValue("?val7", defaultHours);
+                    cmd.Parameters.AddWithValue("?val8", isAdmin);
+                    cmd.Parameters.AddWithValue("?val9", isActive);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Employee Added");
                 }
                 conn.Close();
             }
